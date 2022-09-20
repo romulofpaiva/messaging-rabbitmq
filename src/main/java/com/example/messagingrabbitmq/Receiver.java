5d0a6ed8efbcf8bd791b78c11370d9cd556efe4b
Romulo Ferreira Paiva <romulofpaiva@gmail.com>
2022-09-20 09:45:38 -0300

    Config listener, queue and exchange.

--- src/main/java/com/example/messagingrabbitmq/Receiver.java
+++ src/main/java/com/example/messagingrabbitmq/Receiver.java
@@ -11,7 +11,7 @@ public class Receiver {
     private CountDownLatch latch = new CountDownLatch(1);
 
     public void receiveMessage(String message) {
-        System.out.println("Received < " + message + ">");
+        System.out.println("Received <" + message + ">");
         latch.countDown();
     }
 
